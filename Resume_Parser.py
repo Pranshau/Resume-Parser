c4dbafc5b83fb9c8d156461e2840ac6f617351ba
Pranshau <118929435+Pranshau@users.noreply.github.com>
2023-08-02 16:21:47 +0530

    Resume_Parser.py

--- Resume_Parser.py
+++ Resume_Parser.py
@@ -5,7 +5,6 @@ from nltk.corpus import stopwords
 nltk.download("punkt")
 nltk.download("stopwords")
 
-
 def preprocess_text(text):
     # Tokenize the text into words
     words = word_tokenize(text.lower())
@@ -16,7 +15,6 @@ def preprocess_text(text):
 
     return words
 
-
 def count_keywords(text, keywords):
     # Preprocess the text and keywords
     processed_text = preprocess_text(text)
@@ -26,7 +24,6 @@ def count_keywords(text, keywords):
     keyword_count = sum(1 for word in processed_text if word in processed_keywords)
 
     return keyword_count
-
 
 def find_best_candidate(job_description, candidate_resumes):
     best_candidate = None
@@ -40,37 +37,37 @@ def find_best_candidate(job_description, candidate_resumes):
 
     return best_candidate
 
-
 if __name__ == "__main__":
+    # Roles input
+    roles_input = input("Enter the roles you want applicants for (comma-separated): ")
+    roles = [role.strip() for role in roles_input.split(",")]
+
     # Job description
     job_description = input("Enter the job description: ")
 
-    # Candidate resumes
+    # Candidate resumes with modified names
     candidate_resumes = {
-        "John Doe": "Full-stack developer experienced in Python, Django, JavaScript, and React.",
-        "Jane Smith": "Java developer with expertise in Spring Boot and Hibernate.",
-        "Bob Johnson": "Front-end developer skilled in HTML, CSS, and JavaScript.",
-        "Alice Brown": "Python developer with a focus on data analysis and machine learning.",
-        "Michael Lee": "Backend developer experienced in Java and Spring Framework.",
+        "John Smith": "Full-stack developer experienced in Python, Django, JavaScript, and React.",
+        "Jane Johnson": "Java developer with expertise in Spring Boot and Hibernate.",
+        "Bob White": "Front-end developer skilled in HTML, CSS, and JavaScript.",
+        "Alice Green": "Python developer with a focus on data analysis and machine learning.",
+        "Michael Brown": "Backend developer experienced in Java and Spring Framework.",
         "Sophia Wilson": "Software engineer with experience in C++ and Linux system programming.",
-        "Oliver Martinez": "Web developer proficient in PHP, Laravel, and MySQL.",
-        "Emma Taylor": "Mobile app developer with expertise in Android and Kotlin.",
-        "William Clark": "Game developer skilled in Unity and C#.",
-        "Ava Rodriguez": "Embedded systems developer experienced in C and RTOS.",
+        "Oliver Taylor": "Web developer proficient in PHP, Laravel, and MySQL.",
+        "Emma Clark": "Mobile app developer with expertise in Android and Kotlin.",
+        "William Rodriguez": "Game developer skilled in Unity and C#.",
+        "Ava Martinez": "Embedded systems developer experienced in C and RTOS.",
         "James Evans": "DevOps engineer with expertise in Docker and Kubernetes.",
         "Emily Hernandez": "Database administrator experienced in SQL and MongoDB.",
         "Ethan Moore": "UI/UX designer with a focus on user-centered design principles.",
         "Madison Garcia": "Quality assurance engineer skilled in automated testing using Selenium.",
-        "Olivia Martinez": "Software architect with extensive experience in system design and scalability.",
+        "Olivia Perez": "Software architect with extensive experience in system design and scalability.",
         "Noah Robinson": "Blockchain developer with expertise in Ethereum and Solidity.",
         "Isabella Stewart": "Cloud engineer experienced in AWS services and serverless architecture.",
         "Liam Murphy": "AR/VR developer with proficiency in Unity and Unreal Engine.",
         "Mia Reed": "Big data engineer skilled in Hadoop and Spark.",
         "Elijah Brooks": "Security analyst experienced in ethical hacking and vulnerability assessments.",
     }
-
-    roles_input = input("Enter the roles you want applicants for (comma-separated): ")
-    roles = [role.strip() for role in roles_input.split(",")]
 
     filtered_candidates = {name: resume for name, resume in candidate_resumes.items() if
                            any(role.lower() in resume.lower() for role in roles)}
